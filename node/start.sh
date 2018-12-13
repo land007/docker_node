ece2afa3433a985da9164cd0ace040a2252396c4
Jia Yiqiu <jiayiqiu@eyecool.cn>
2018-12-13 11:27:30 +0800

    test

--- node/start.sh
+++ node/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/usr/bin/nohup supervisor -w /node/ /node/main.js & > /node/node.out 2>&1 & bash
+#/usr/bin/nohup supervisor -w /node/ /node/main.js & > /node/node.out 2>&1 & bash
+supervisor -w /node/ /node/main.js & > /node/node.out 2>&1
