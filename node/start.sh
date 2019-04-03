bcdd132b59144d78352d9aec055da63248b2d066
Yiqiu Jia <yiqiujia@hotmail.com>
2019-04-04 00:18:06 +0800

    test

--- node/start.sh
+++ node/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#/usr/bin/nohup supervisor -w /node/ /node/main.js > /node/node.out 2>&1 & bash
-supervisor -w /node/ /node/main.js
+#supervisor -w /node/ /node/server.js
+/usr/bin/nohup supervisor -w /node/ /node/server.js > /node/node.out 2>&1 & bash
